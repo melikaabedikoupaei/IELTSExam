c525a6333df271eae6d21726afe64395ebb893cf
melika abedi <melikaabedi681375@gmail.com>
2023-10-05 15:21:17 +0200

    make opp beter

--- IELTSExam.py
+++ IELTSExam.py
@@ -1,49 +1,62 @@
-from bs4 import BeautifulSoup 
-import requests 
-import smtplib 
+import requests
+from bs4 import BeautifulSoup
+import smtplib
 from email.mime.multipart import MIMEMultipart
-class IeltsExam:
+import logging
+
+class IeltsScraper:
     def __init__(self):
-        self.nothing_new=['بر اساس جستجوی شما هیچ آزمونی پیدا نشد. لطفا جستجوی انجام شده خود را اصلاح نمایید.\n']
-        self.exam_info = "" 
-        self.message_body=""
-        self.gmail_user = 'melikaabedi681375@gmail.com'  
-        self.gmail_password = 'sckl gryk bpkx mfgj'
-        self.receiver_email_address='ghazalrezaei2001@gmail.com' 
-    def request_for_information(self):
-        r = requests.get("https://irsafam.com/ielts/timetable?city%5B%5D=tehran&model%5B%5D=ielts&type%5B%5D=academic") 
-        soup = BeautifulSoup(r.text, 'html.parser') 
-        result = soup.find_all('div', attrs = {'class':'exam'})
-        return result 
-    def body_of_email(self):
-        results = self.request_for_information()
-        for i in results: 
-            self.exam_info += i.text.strip() 
-            self.exam_info+= "\n"
-        self.exam_info=[self.exam_info]
-        if self.exam_info==self.nothing_new:
-            self.message_body="nopeeee baby nothing changes"
-        else:
-            self.message_body="check the irsafam.com website "
-        return self.message_body
-    def send_email(self):
+        pass
+
+    def fetch_exam_info(self):
+        url = "https://irsafam.com/ielts/timetable?city%5B%5D=tehran&model%5B%5D=ielts&type%5B%5D=academic"
         try:
-            sent_from = self.gmail_user
-            to = [self.receiver_email_address]
-            msg = """Subject:IeltsExam """
-            msg += self.body_of_email()
+            r = requests.get(url)
+            r.raise_for_status()
+            soup = BeautifulSoup(r.text, 'html.parser')
+            results = soup.find_all('div', attrs={'class': 'exam'})
+            return [result.text.strip() for result in results]
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Failed to fetch exam info: {e}")
+            return []
+
+class EmailSender:
+    def __init__(self, sender_email, sender_password, receiver_email):
+        self.sender_email = sender_email
+        self.sender_password = sender_password
+        self.receiver_email = receiver_email
+
+    def send_email(self, subject, message_body):
+        try:
+            msg = """Subject:IeltsExam """ 
+            msg += message_body
+
+
             server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
             server.ehlo()
-            server.login(self.gmail_user, self.gmail_password)
-            server.sendmail(sent_from, to, msg)
+            server.login(self.sender_email, self.sender_password)
+            server.sendmail(self.sender_email, [self.receiver_email], msg)
             server.close()
-            print("email sent successfully")
+            logging.info("Email sent successfully")
         except Exception as e:
-            print("An error occurred while sending the email:", e)
-
-email_sender = IeltsExam()
-email_sender.send_email()
+            logging.error("An error occurred while sending the email:", e)
 
 
+def main():
+    scraper = IeltsScraper()
+    exam_info = scraper.fetch_exam_info()
+    nothing_new=['بر اساس جستجوی شما هیچ آزمونی پیدا نشد. لطفا جستجوی انجام شده خود را اصلاح نمایید.']
+    if exam_info==nothing_new:
+        message_body = "No new exams found.baby.I love you "
+    else:
+        message_body = "check irsafam website baybe.kiss"
 
- 
+    sender_email = 'melikaabedi681375@gmail.com'
+    sender_password = 'sckl gryk bpkx mfgj' 
+    receiver_email = 'melikaabedi681375@gmail.com'
+
+    email_sender = EmailSender(sender_email, sender_password, receiver_email)
+    email_sender.send_email("IeltsExam", message_body)
+
+if __name__ == "__main__":
+    main()
