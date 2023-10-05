43abd6b7a34677bdd11b086dab337186e0db1505
melika abedi <melikaabedi681375@gmail.com>
2023-10-05 13:37:17 +0200

    edit code with oop method

--- IELTSExam.py
+++ IELTSExam.py
@@ -1,42 +1,50 @@
-###https://thecaferobot.com/learn/web-scraping-raspberry-pi-python-tutorial-guide/
 from bs4 import BeautifulSoup 
 import requests 
 import re 
 import smtplib 
 from email.mime.multipart import MIMEMultipart
-nothing_new=['بر اساس جستجوی شما هیچ آزمونی پیدا نشد. لطفا جستجوی انجام شده خود را اصلاح نمایید.\n']
-exam_info = "" 
-message_body=""
-r = requests.get("https://irsafam.com/ielts/timetable?city%5B%5D=tehran&model%5B%5D=ielts&type%5B%5D=academic") 
-soup = BeautifulSoup(r.text, 'html.parser') 
-result = soup.find_all('div', attrs = {'class':'exam'}) 
- 
-for i in result: 
-    exam_info += i.text.strip() 
-    exam_info += "\n"
-exam_info=[exam_info]
-gmail_user = 'melikaabedi681375@gmail.com'  
-gmail_password = 'sckl gryk bpkx mfgj'   #Enter your Email pasword 
-sent_from = gmail_user   
-to = ['melikaabedi681375@gmail.com']      #list of destination Email address 
+class IeltsExam:
+    def __init__(self):
+        self.nothing_new=['بر اساس جستجوی شما هیچ آزمونی پیدا نشد. لطفا جستجوی انجام شده خود را اصلاح نمایید.\n']
+        self.exam_info = "" 
+        self.message_body=""
+        self.gmail_user = 'melikaabedi681375@gmail.com'  
+        self.gmail_password = 'sckl gryk bpkx mfgj'
+        self.receiver_email_address='melikaabedi681375@gmail.com' 
+    def request_for_information(self):
+        r = requests.get("https://irsafam.com/ielts/timetable?city%5B%5D=tehran&model%5B%5D=ielts&type%5B%5D=academic") 
+        soup = BeautifulSoup(r.text, 'html.parser') 
+        result = soup.find_all('div', attrs = {'class':'exam'})
+        return result 
+    def body_of_email(self):
+        results = self.request_for_information()
+        for i in results: 
+            self.exam_info += i.text.strip() 
+            self.exam_info+= "\n"
+        self.exam_info=[self.exam_info]
+        if self.exam_info==self.nothing_new:
+            self.message_body="nopeeee baby nothing changes"
+        else:
+            self.message_body="check the irsafam.com website "
+        return self.message_body
+    def send_email(self):
+        try:
+            sent_from = self.gmail_user
+            to = [self.receiver_email_address]
+            msg = """Subject:IeltsExam """
+            msg += self.body_of_email()
+            server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            server.ehlo()
+            server.login(self.gmail_user, self.gmail_password)
+            server.sendmail(sent_from, to, msg)
+            server.close()
+            print("email sent successfully")
+        except Exception as e:
+            print("An error occurred while sending the email:", e)
 
-    #Email title 
-msg = """Subject:IeltsExam 
-    """ 
-    #add news to email massage 
-message_body=""
-if exam_info==nothing_new:
-    message_body="nopeeee baby nothing changes"
-else:
-    message_body="check the irsafam.com website " 
-msg += message_body
+email_sender = IeltsExam()
+email_sender.send_email()
 
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465) 
-server.ehlo() 
-server.login(gmail_user, gmail_password) 
-server.sendmail(sent_from, to, msg) 
-server.close() 
 
-print ('Email sent!') 
 
  
--- README.txt
+++ README.txt
@@ -4,3 +4,5 @@ if you want to send an email you need app password you can see the guide in belo
 https://support.google.com/accounts/answer/185833?visit_id=638320481653487493-3027603714&p=InvalidSecondFactor&rd=1
 for automaticly send the emails 
 https://docs.github.com/en/actions/quickstart
+https://www.bing.com/videos/riverview/relatedvideo?&q=automate+runing+code+with+github&&mid=3E9AF6595E37A36FE7273E9AF6595E37A36FE727&&FORM=VRDGAR
+
